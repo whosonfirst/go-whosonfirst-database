12e15ac76ecd92c5ff5d1ff844320f1a50d0e647
thisisaaronland <thisisaaronland@localhost>
2025-09-11 18:08:00 -0700

    snapshot: block out prune app; still doesn't compile

---
+++ app/sql/tables/prune/flags.go
@@ -0,0 +1,55 @@
+package prune
+
+import (
+	"flag"
+
+	"github.com/sfomuseum/go-flags/flagset"
+)
+
+var database_uri string
+var iterator_uri string
+
+var all bool
+var ancestors bool
+var concordances bool
+var geojson bool
+var spelunker bool
+var geometries bool
+var names bool
+var rtree bool
+var properties bool
+var search bool
+var spr bool
+var supersedes bool
+
+var spatial_tables bool
+var spelunker_tables bool
+
+var verbose bool
+
+func DefaultFlagSet() *flag.FlagSet {
+
+	fs := flagset.NewFlagSet("index")
+
+	fs.StringVar(&database_uri, "database-uri", "", "...")
+	fs.StringVar(&iterator_uri, "iterator-uri", "repo://", "...")
+
+	fs.BoolVar(&all, "all", false, "Index all tables (except the 'search' and 'geometries' tables which you need to specify explicitly)")
+	fs.BoolVar(&ancestors, "ancestors", false, "Index the 'ancestors' tables")
+	fs.BoolVar(&concordances, "concordances", false, "Index the 'concordances' tables")
+	fs.BoolVar(&geojson, "geojson", false, "Index the 'geojson' table")
+	fs.BoolVar(&spelunker, "spelunker", false, "Index the 'spelunker' table")
+	fs.BoolVar(&geometries, "geometries", false, "Index the 'geometries' table (requires that libspatialite already be installed)")
+	fs.BoolVar(&names, "names", false, "Index the 'names' table")
+	fs.BoolVar(&rtree, "rtree", false, "Index the 'rtree' table")
+	fs.BoolVar(&properties, "properties", false, "Index the 'properties' table")
+	fs.BoolVar(&search, "search", false, "Index the 'search' table (using SQLite FTS4 full-text indexer)")
+	fs.BoolVar(&spr, "spr", false, "Index the 'spr' table")
+	fs.BoolVar(&supersedes, "supersedes", false, "Index the 'supersedes' table")
+
+	fs.BoolVar(&spatial_tables, "spatial-tables", false, "If true then index the necessary tables for use with the whosonfirst/go-whosonfirst-spatial-sqlite package.")
+	fs.BoolVar(&spelunker_tables, "spelunker-tables", false, "If true then index the necessary tables for use with the whosonfirst/go-whosonfirst-spelunker packages")
+
+	fs.BoolVar(&verbose, "verbose", false, "Enable verbose (debug) logging")
+	return fs
+}
---
+++ app/sql/tables/prune/prune.go
@@ -0,0 +1,208 @@
+package prune
+
+import (
+	"context"
+	"flag"
+	"fmt"
+	"log/slog"
+
+	database_sql "github.com/sfomuseum/go-database/sql"
+	"github.com/sfomuseum/go-flags/flagset"
+	"github.com/whosonfirst/go-whosonfirst-database/sql/prune"
+	"github.com/whosonfirst/go-whosonfirst-database/sql/tables"
+)
+
+func Run(ctx context.Context) error {
+	fs := DefaultFlagSet()
+	return RunWithFlagSet(ctx, fs)
+}
+
+// To do: Add RunWithOptions...
+
+func RunWithFlagSet(ctx context.Context, fs *flag.FlagSet) error {
+
+	flagset.Parse(fs)
+
+	if verbose {
+		slog.SetLogLoggerLevel(slog.LevelDebug)
+		slog.Debug("Verbose logging enabled")
+	}
+
+	if spatial_tables {
+		rtree = true
+		geojson = true
+		properties = true
+		spr = true
+	}
+
+	if spelunker_tables {
+		// rtree = true
+		spr = true
+		spelunker = true
+		geojson = true
+		concordances = true
+		ancestors = true
+		search = true
+	}
+
+	db, err := database_sql.OpenWithURI(ctx, database_uri)
+
+	if err != nil {
+		return err
+	}
+
+	defer func() {
+
+		err := db.Close()
+
+		if err != nil {
+			slog.Error("Failed to close database connection", "error", err)
+		}
+	}()
+
+	to_prune := make([]database_sql.Table, 0)
+
+	if geojson || all {
+
+		gt, err := tables.NewGeoJSONTableWithDatabase(ctx, db)
+
+		if err != nil {
+			return fmt.Errorf("failed to create '%s' table because %s", tables.GEOJSON_TABLE_NAME, err)
+		}
+
+		to_prune = append(to_prune, gt)
+	}
+
+	if supersedes || all {
+
+		t, err := tables.NewSupersedesTableWithDatabase(ctx, db)
+
+		if err != nil {
+			return fmt.Errorf("failed to create '%s' table because %s", tables.SUPERSEDES_TABLE_NAME, err)
+		}
+
+		to_prune = append(to_prune, t)
+	}
+
+	if rtree || all {
+
+		gt, err := tables.NewRTreeTableWithDatabase(ctx, db)
+
+		if err != nil {
+			return fmt.Errorf("failed to create 'rtree' table because %s", err)
+		}
+
+		to_prune = append(to_prune, gt)
+	}
+
+	if properties || all {
+
+		gt, err := tables.NewPropertiesTableWithDatabase(ctx, db)
+
+		if err != nil {
+			return fmt.Errorf("failed to create 'properties' table because %s", err)
+		}
+
+		to_prune = append(to_prune, gt)
+	}
+
+	if spr || all {
+
+		st, err := tables.NewSPRTableWithDatabase(ctx, db)
+
+		if err != nil {
+			return fmt.Errorf("failed to create '%s' table because %s", tables.SPR_TABLE_NAME, err)
+		}
+
+		to_prune = append(to_prune, st)
+	}
+
+	if spelunker || all {
+
+		st, err := tables.NewSpelunkerTableWithDatabase(ctx, db)
+
+		if err != nil {
+			return fmt.Errorf("failed to create '%s' table because %s", tables.SPELUNKER_TABLE_NAME, err)
+		}
+
+		to_prune = append(to_prune, st)
+	}
+
+	if names || all {
+
+		nm, err := tables.NewNamesTableWithDatabase(ctx, db)
+
+		if err != nil {
+			return fmt.Errorf("failed to create '%s' table because %s", tables.NAMES_TABLE_NAME, err)
+		}
+
+		to_prune = append(to_prune, nm)
+	}
+
+	if ancestors || all {
+
+		an, err := tables.NewAncestorsTableWithDatabase(ctx, db)
+
+		if err != nil {
+			return fmt.Errorf("failed to create '%s' table because %s", tables.ANCESTORS_TABLE_NAME, err)
+		}
+
+		to_prune = append(to_prune, an)
+	}
+
+	if concordances || all {
+
+		cn, err := tables.NewConcordancesTableWithDatabase(ctx, db)
+
+		if err != nil {
+			return fmt.Errorf("failed to create '%s' table because %s", tables.CONCORDANCES_TABLE_NAME, err)
+		}
+
+		to_prune = append(to_prune, cn)
+	}
+
+	// see the way we don't check all here - that's so people who don't have
+	// spatialite installed can still use all (20180122/thisisaaronland)
+
+	if geometries {
+
+		gm, err := tables.NewGeometriesTableWithDatabase(ctx, db)
+
+		if err != nil {
+			return fmt.Errorf("failed to create '%s' table because %v", tables.CONCORDANCES_TABLE_NAME, err)
+		}
+
+		to_prune = append(to_prune, gm)
+	}
+
+	// see the way we don't check all here either - that's because this table can be
+	// brutally slow to index and should probably really just be a separate database
+	// anyway... (20180214/thisisaaronland)
+
+	if search {
+
+		// ALT FILES...
+
+		st, err := tables.NewSearchTableWithDatabase(ctx, db)
+
+		if err != nil {
+			return fmt.Errorf("failed to create 'search' table because %v", err)
+		}
+
+		to_prune = append(to_prune, st)
+	}
+
+	if len(to_prune) == 0 {
+		return fmt.Errorf("You forgot to specify which (any) tables to prune")
+	}
+
+	for _, iterator_source := range fs.Args() {
+		err := prune.PruneTablesWithIterator(ctx, iterator_uri, iterator_source, db, to_prune...)
+
+		if err != nil {
+			return fmt.Errorf("Failed to iterate %s, %w", iterator_source, err)
+		}
+	}
+
+	return nil
+}
